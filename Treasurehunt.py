07add6a38136268c0adcb9a232bf722ccf5a9c60
vansh69420 <117568183+vansh69420@users.noreply.github.com>
2023-02-09 00:09:33 +0530

    Add files via upload

---
+++ Treasurehunt.py
@@ -0,0 +1,50 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+route=input("Which path you choose Left or Right?\n")
+path=route.lower()
+
+if path=="left":
+  path2=input("Now you arrived at docs of a river ,Would you like to wait for the boat Type 'wait' or Tpye 'swim' if you want to swim through river.\n")
+  lower_case_path2=path2.lower()
+  if lower_case_path2=="wait":
+    door=input("Noice you arrived on the anonymous island unharmed now you see three doors REd,YELLOW,BLUE which you want to choose\n")
+    lower_case_door=door.lower()
+    if lower_case_door=="red" or lower_case_door=="yellow":
+      print("Game is over , You choose your own death.")
+    else:
+      print("Congrats you got the treasure.")
+  else:
+    print("Your Game is over , You became the Breakfast of a shark.")
+    
+else:
+  print("Your game is over , you caught by an animal.")
+  
+
+
+
